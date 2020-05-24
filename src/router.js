019481c004c7c6863a4296347e960f8d54686956
John Nathan Albrinck <john.n.albrinck.20@dartmouth.edu>
2020-05-24 15:28:40 -0700

    Move server and router to api repo

--- src/router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const router = Router();
-
-router.get('/', (req, res) => {
-  res.json({ message: 'welcome to our 2ManyTabz api!' });
-});
-
-export default router;
