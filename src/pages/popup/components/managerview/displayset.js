39488f1cd425234b29baee65b0846eb423578a67
Katherine Taylor <katherine.j.taylor.22@dartmouth.edu>
2020-06-08 16:09:42 -0400

    changed radio buttons to toggle-esque switch

--- src/pages/popup/components/managerview/displayset.js
+++ src/pages/popup/components/managerview/displayset.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import { connect } from 'react-redux';
 import { switchView } from '../../../../shared/actions/preferenceactions';
@@ -6,8 +7,12 @@ const DisplaySetting = (props) => {
   return (
     <div id="display-setting">
       <input type="text" placeholder="Search for a tab..." onChange={(e) => { props.setFilter(e.target.value ? { title: e.target.value } : {}); }} />
-      <input type="radio" value="0" name="viewType" checked={props.displayType === '0'} onChange={(e) => { props.switchView(e.target.value); }} />
-      <input type="radio" value="1" name="viewType" checked={props.displayType === '1'} onChange={(e) => { props.switchView(e.target.value); }} />
+
+      <input type="radio" id="listbutton" value="0" name="viewType" checked={props.displayType === '0'} onChange={(e) => { props.switchView(e.target.value); }} />
+      <label htmlFor="listbutton" id="listlabel">List</label>
+
+      <input type="radio" id="gridbutton" value="1" name="viewType" checked={props.displayType === '1'} onChange={(e) => { props.switchView(e.target.value); }} />
+      <label htmlFor="gridbutton">Grid</label>
     </div>
   );
 };
